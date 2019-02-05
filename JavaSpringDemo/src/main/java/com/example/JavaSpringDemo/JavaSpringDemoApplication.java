f01a2a26f088b951d04abe53e372ee2c3f38883f
Dingchingger <dingchingger@hotmail.com>
2019-02-04 19:45:55 -0500

    project init

---
+++ JavaSpringDemo/src/main/java/com/example/JavaSpringDemo/JavaSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.JavaSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDemoApplication.class, args);
+	}
+
+}
+
